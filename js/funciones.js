340d983008d356b63d138e6437dc82cdc4e49a81
SchatzM <bri@npass.us>
2020-06-08 23:03:08 +0200

    Añadida lógica JS

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,21 @@
+/**
+ * Práctica 14
+ * Modificar Imagen
+ * Brian Passos
+ */
 'use strict';
+
+const	elemento = {
+			imagen: document.createElement ('img',),
+			insertar: document.querySelector ('section .container figure')
+		},
+		zoomImagen = () => {
+			elemento.imagen.src = 'img/Práctica02.png';
+			elemento.insertar.appendChild(elemento.imagen);
+			elemento.imagen.addEventListener ('click', () => {
+				elemento.insertar.classList.toggle ('zoom');
+				elemento.insertar.style.width = elemento.insertar.classList.contains ('zoom') ? '800px' : '128px';
+			});
+		};
+
+zoomImagen ();
