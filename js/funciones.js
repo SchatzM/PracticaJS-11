15f66bf8030dac5ebe9635daa21e10a193d70b67
SchatzM <bri@npass.us>
2020-06-08 23:10:17 +0200

    Añadidos comentarios

--- js/funciones.js
+++ js/funciones.js
@@ -5,17 +5,17 @@
  */
 'use strict';
 
-const	elemento = {
-			imagen: document.createElement ('img',),
-			insertar: document.querySelector ('section .container figure')
+const	elemento = { // Objeto con referencia a los elementos que usaremos
+			imagen: document.createElement ('img'), // Creamos el elemento para la imagen
+			insertar: document.querySelector ('section .container figure') // Elemento donde insertaremos el elemento de imagen anterior creado
 		},
-		zoomImagen = () => {
-			elemento.imagen.src = 'img/Práctica02.png';
-			elemento.insertar.appendChild(elemento.imagen);
-			elemento.imagen.addEventListener ('click', () => {
-				elemento.insertar.classList.toggle ('zoom');
-				elemento.insertar.style.width = elemento.insertar.classList.contains ('zoom') ? '800px' : '128px';
+		zoomImagen = () => { // Función que alterna zoom en la imagen
+			elemento.imagen.src = 'img/Práctica02.png'; // Agregamos el source de la imagen al elemento
+			elemento.insertar.appendChild(elemento.imagen); // Añadimos el elemento de imagen al figure dentro de section
+			elemento.imagen.addEventListener ('click', () => { // Función ejecutada al hacer clic sobre la imagen
+				elemento.insertar.classList.toggle ('zoom'); // Alternamos la clase 'zoom' sobre el elemento que contiene la imagen para identificar el estado del zoom
+				elemento.insertar.style.width = elemento.insertar.classList.contains ('zoom') ? '800px' : '128px'; // Alternamos el ancho del elemento que contiene la imagen dependiendo de si la clase 'zoom' antes mencionada existe o no
 			});
 		};
 
-zoomImagen ();
+zoomImagen (); // Llamamos a la función principal
